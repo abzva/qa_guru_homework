a5a797aa0bb42031156416fa5c960167c5e0e112
Anastasuy092 <bazovaanastasia@yandex.ru>
2026-06-22 18:22:43 +0300

    refactor: add test data for positive02, fix setup/teardown per test

--- Student01Prj01/automation_practice_form_test_suite.py
+++ Student01Prj01/automation_practice_form_test_suite.py
@@ -2,10 +2,9 @@ import os
 import automation_practice_form_po
 
 class AutomationPracticeFormTestSuite:
-    def __init__(self):
-        self.automation_practice_form = automation_practice_form_po.AutomationPracticeFormPO("https://qa-guru.github.io/one-page-form/automation-practice-form.html")
 
     def setup(self):
+        self.automation_practice_form = automation_practice_form_po.AutomationPracticeFormPO("https://qa-guru.github.io/one-page-form/automation-practice-form.html")
         self.automation_practice_form.setup()
         self.tmp_file_name = self._create_tmp_file()
 
@@ -17,12 +16,15 @@ class AutomationPracticeFormTestSuite:
     
     def test_form_positive01(self):
         self.automation_practice_form.fill_in_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
-        self.automation_practice_form.assert_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
+        self.automation_practice_form.assert_student_name('Dmitry', 'Bugaev')
+        self.automation_practice_form.assert_email('bugaev@example.com')
+        self.automation_practice_form.assert_result_is_visible()
 
     def test_form_positive02(self):
-        # TODO: придумай другой набор тестовых данных
-        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
-        self.automation_practice_form.assert_form(self.tmp_file_name, "Dmitry","Bugaev", "bugaev@example.com", "Male","1234567890",("1988", "4", "22"),("Maths", "English"), ("Sports", "Music"), "г. Санкт-Петербург, ул. Невский проспект, д 101", "NCR",  "Noida")
+        self.automation_practice_form.fill_in_form(self.tmp_file_name, "Lena","Petrova", "petrovlen@example.com", "Female", "9876543210", ("1995", "7", "15"), ("Chemistry", "Physics"), ("Reading", "Music"), "г. Москва, ул. Арбат, д 25", "Uttar Pradesh", "Agra")
+        self.automation_practice_form.assert_student_name('Lena', 'Petrova')
+        self.automation_practice_form.assert_email('petrovlen@example.com')
+        self.automation_practice_form.assert_result_is_visible()
 
     def tear_down(self):
         if os.path.exists(self.tmp_file_name):
@@ -35,7 +37,11 @@ test_suite = AutomationPracticeFormTestSuite()
 try:
     test_suite.setup()
     test_suite.test_form_positive01()
-    # TODO: в общем виде работать не будет, и в нашем виде работать не будет, так как setup и tear_down должны вызываться перед каждым тестом ... так же в идеале перед каждым тестом создается PO - подумайте как решить малой кровью
+finally:
+    test_suite.tear_down()
+
+try:
+    test_suite.setup()
     test_suite.test_form_positive02()
 finally:
     test_suite.tear_down()
